e818c125d9ae40fdf777f504c740cb5adc030b35
Valera Novotochin <manson413@gmail.com>
2019-03-28 23:18:53 +0200

    add slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 window.onload = function(){
-	console.log('load');
+	$('.socks-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg class="icon-sprite svg-icon"><use xlink:href="img/symbol_sprite.svg#icon-angle-left-solid"></use></svg></button>',
+		nextArrow: '<button type="button" class="slick-arrow slick-next"><svg class="icon-sprite svg-icon"><use xlink:href="img/symbol_sprite.svg#icon-angle-right-solid"></use></svg></button>'
+	});
 };
